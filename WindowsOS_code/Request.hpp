df01d38e26eb963d463772c621c40cdd4151f627
Pavlos Sakoglou <23369146+PavlosSakoglou@users.noreply.github.com>
2018-03-02 10:46:04 -0500

    Fixed typo

--- WindowsOS_code/Request.hpp
+++ WindowsOS_code/Request.hpp
@@ -1,8 +1,8 @@
 /*
-*	© Superharmonic Technologies
+*	Â© Superharmonic Technologies
 *	Pavlos Sakoglou
 *
-*  ================================================
+*  =================================================
 *
 *	Request hierarchy definition
 *
